cf7dbcdd0416eca018819ef618f6037d20a76bc4
René Wirnata <rene.wirnata@pandascience.net>
2022-12-19 23:18:16 +0100

    backup: add notifications and align with UTC time

--- bin/syncoid.sh
+++ bin/syncoid.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/bash
 
 # crontab -e
-# 1 21 * * *     /usr/bin/bash /home/rene/bin/syncoid.sh &>> /var/log/syncoid.log
+# CRON_TZ=UTC
+# 0 19 * * *     /usr/bin/bash /home/rene/bin/syncoid.sh &>> /var/log/syncoid.log
 
 echo -e "\n\nStarting syncoid backup: $(date +"%F @ %T")"
 syncoid \
 	--sshkey=/home/rene/.ssh/backup_awow \
 	--recursive --sendoptions="w" \
 	zroot/encr root@192.168.178.81:backup/work/encr
-echo "Finished syncoid backup: $(date +"%F @ %T")"
+
+EXITCODE=$?
+if [[ ${EXITCODE} != 0 ]]; then
+	sudo -u rene DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus \
+	/usr/bin/notify-send -u critical -i /SHARE/backup.png "Backup failed!"
+	echo "Syncoid backup failed!: $(date +"%F @ %T")"
+
+else
+	sudo -u rene DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus \
+	/usr/bin/notify-send -t 30000 -u normal -i /SHARE/backup.png "Backup completed!"
+	echo "Finished syncoid backup: $(date +"%F @ %T")"
+fi
+
+exit "${EXITCODE}"
