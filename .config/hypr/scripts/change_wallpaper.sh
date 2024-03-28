bf1b8b893038b3b5306cbc708ecc2ed950c15f11
René Wirnata <rene.wirnata@pandascience.net>
2024-03-28 22:45:53 +0100

    hypr: switch to hyprland-native lock tools
    Replaced (incl. configs):
    - swaylock -> hyprlock
    - swayidle -> hypridle
    
    Lead to new freezes on suspended or turned-off screens, so re-activated
    kanshi.
    
    In total this change reduced [red screens](https://github.com/hyprwm/Hyprland/issues/3673)
    a lot, but still see freezes sometimes.
    
    Hyprlock only supports png images, e.g. for backgound, so adapt
    wallpaper change script to convert current image to png instead of
    creating a simple copy.

--- .config/hypr/scripts/change_wallpaper.sh
+++ .config/hypr/scripts/change_wallpaper.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-wallcache="${HOME}/.cache/current_wallpaper.jpg"
+wallcache="${HOME}/.cache/current_wallpaper.png"
 wallpath="${HOME}/Pictures/wallpapers/"
 
 # use random wallpaper from path
 wallpaper="$(find $wallpath | shuf -n1)"
 echo $wallpaper
-# create copy in cache for easier referemce, e.g. by swaylock
-cp "$wallpaper" "$wallcache"
+# create png copy in cache for easier referemce, e.g. by swaylock or hyprlock
+convert "$wallpaper" "$wallcache"
 # set new wallpaper
 swww img "$wallpaper" --transition-step 40 --transition-fps 60 --transition-type any
 # generate new color palette
--- .config/hypr/scripts/screenlock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# https://github.com/swaywm/swayidle/blob/master/swayidle.1.scd#example
-# BUG: https://github.com/hyprwm/Hyprland/issues/4134#issuecomment-1871464111
-pkill swayidle
-swayidle -w \
-  timeout 180 'swaylock -f' \
-  timeout 600 'hyprctl dispatch dpms off' \
-  resume 'hyprctl dispatch dpms on' \
-  before-sleep 'sh -c "swaylock -f && sleep 3"'
