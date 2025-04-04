955b11cae711ed0f3a81b7a9af17004407e26558
René Wirnata <rene.wirnata@pandascience.net>
2025-04-04 16:25:31 +0200

    bar: add dunst status script

---
+++ .config/waybar/scripts/dunst-status.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# return dunst state as JSON according to:
+# https://github.com/Alexays/Waybar/wiki/Module:-Custom#module-custom-config-return-type
+
+readonly COUNT="$(dunstctl count waiting)"
+readonly PAUSED="$(dunstctl is-paused)"
+
+if ((COUNT > 0)); then
+    printf '{"alt": "paused", "text": " (%s)"}' ${COUNT}
+elif [[ ${PAUSED} == "true" ]]; then
+    echo '{"alt": "paused"}'
+else
+    echo '{"alt": "unpaused"}'
+fi
