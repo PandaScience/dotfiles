0d7204eb1cd4c05bf65d4dd6ecfcefe7be3cab0d
René Wirnata <rene.wirnata@pandascience.net>
2025-04-30 22:50:48 +0200

    hypr: update wallpaper script

--- .config/hypr/scripts/change_wallpaper.sh
+++ .config/hypr/scripts/change_wallpaper.sh
@@ -5,11 +5,10 @@ wallpath="${HOME}/Pictures/wallpapers/"
 
 # use random wallpaper from path
 wallpaper="$(find $wallpath | shuf -n1)"
-echo $wallpaper
 # set new wallpaper
 swww img "$wallpaper" --transition-step 40 --transition-fps 60 --transition-type any
 # create png copy in cache for easier referemce, e.g. by swaylock or hyprlock
-convert "$wallpaper" "$wallcache"
+magick "$wallpaper" "$wallcache"
 # generate new color palette
 sleep 2
 wal -ni "$wallpaper"
