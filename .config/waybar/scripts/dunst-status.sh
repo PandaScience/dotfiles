71882a8479ca7adf3d228aa6b9b6d2f2a0c70e86
René Wirnata <rene.wirnata@pandascience.net>
2025-04-30 22:49:49 +0200

    bar: fix dunst script

--- .config/waybar/scripts/dunst-status.sh
+++ .config/waybar/scripts/dunst-status.sh
@@ -2,10 +2,9 @@
 # return dunst state as JSON according to:
 # https://github.com/Alexays/Waybar/wiki/Module:-Custom#module-custom-config-return-type
 
-readonly COUNT
-readonly PAUSED
-COUNT="$(dunstctl count waiting)"
-PAUSED="$(dunstctl is-paused)"
+# shellcheck disable=SC2155
+readonly COUNT="$(dunstctl count waiting)"
+readonly PAUSED="$(dunstctl is-paused)"
 
 if ((COUNT > 0)); then
     printf '{"alt": "paused", "text": " (%s)"}' "${COUNT}"
