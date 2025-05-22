6f2f1572cc3fafd8c21b8dd7a76f394da5864d66
René Wirnata <rene.wirnata@pandascience.net>
2025-05-22 20:59:35 +0200

    hypr: remove obsolete hy3-movefocus script

--- .config/hypr/scripts/hy3-movefocus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# https://github.com/outfoxxed/hy3/issues/2
-NowWindow="$(hyprctl activewindow -j | jq ".address")"
-
-hyprctl dispatch hy3:movefocus "$1"  # && sleep 0.05
-ThenWindow="$(hyprctl activewindow -j | jq ".address")"
-if [ "$NowWindow" == "$ThenWindow" ]; then
-  hyprctl dispatch movefocus "$1"
-fi
