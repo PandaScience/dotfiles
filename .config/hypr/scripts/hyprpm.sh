f415ce455aeaee980e1d0b27d82650725321aa56
René Wirnata <rene.wirnata@pandascience.net>
2025-05-22 21:00:04 +0200

    hypr: add hyprpm setup script

---
+++ .config/hypr/scripts/hyprpm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# hy3 - i3 layout
+hyprpm add https://github.com/outfoxxed/hy3
+# hyprfocus - flash/resive on focus change
+hyprpm add https://github.com/daxisunder/hyprfocus
+# hyprexpo - client overview
+hyprpm add https://github.com/hyprwm/hyprland-plugins
+# hyprspace - client overview
+hyprpm add https://github.com/KZDKM/Hyprspace
+
+hyprpm enable hy3
+hyprpm enable hyprfocus
+hyprpm enable hyprexpo
+hyprpm enable Hyprspace
