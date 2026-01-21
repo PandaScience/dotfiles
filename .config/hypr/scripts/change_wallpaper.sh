10b62a83303f9f6ef36aad2d7f59a551ca469dcf
René Wirnata <rene.wirnata@pandascience.net>
2026-01-21 18:48:42 +0100

    kitty: replace wal with static colorscheme

--- .config/hypr/scripts/change_wallpaper.sh
+++ .config/hypr/scripts/change_wallpaper.sh
@@ -4,11 +4,9 @@ wallcache="${HOME}/.cache/current_wallpaper.png"
 wallpath="${HOME}/Pictures/wallpapers/"
 
 # use random wallpaper from path
-wallpaper="$(find $wallpath | shuf -n1)"
+wallpaper="$(find "$wallpath" | shuf -n1)"
+
 # set new wallpaper
 swww img "$wallpaper" --transition-step 40 --transition-fps 60 --transition-type any
 # create png copy in cache for easier referemce, e.g. by swaylock or hyprlock
 magick "$wallpaper" "$wallcache"
-# generate new color palette
-sleep 2
-wal -ni "$wallpaper"
