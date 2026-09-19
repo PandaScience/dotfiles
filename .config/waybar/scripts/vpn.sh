a33c7ac56ada75985d563da8e682075a9724b23b
René Wirnata <rene.wirnata@pandascience.net>
2026-09-19 20:04:18 +0200

    bar: add dedicated wireguard module

--- .config/waybar/scripts/vpn.sh
+++ .config/waybar/scripts/vpn.sh
@@ -2,7 +2,7 @@
 # returns active vpn connection names
 
 # fetch all active VPN connections
-VPNs=$(nmcli -f NAME,TYPE con show --active | awk '($2 == "vpn") { print $1 }')
+VPNs=$(nmcli -f NAME,TYPE con show --active | awk '($2 == "vpn" || $2 == "wireguard") { print $1 }')
 # convert lines into comma-separated list
 VPNs=$(echo "$VPNs" | paste -s -d, -)
 
